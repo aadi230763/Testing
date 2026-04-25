2b1916ead392932d60e31ace4b87d758eaf8f8fe
Aditya Chawla <163458650+aadi230763@users.noreply.github.com>
2026-04-25 23:12:37 +0530

    Refactor functions and fix memory issues in app.c

--- app.c
+++ app.c
@@ -3,51 +3,37 @@
 #include <string.h>
 #include <limits.h>
 
-void alpha(char *user) {
-    char query[256];
-    sprintf(query, "SELECT * FROM users WHERE name='%s'", user);
-    printf("%s\n", query);
-}
-
-void beta(char *input) {
-    char buf[8];
+void alpha(char *input) {
+    char buf[10];
     strcpy(buf, input);
     printf("%s\n", buf);
 }
 
-void gamma() {
-    char *p = (char*)malloc(20);
-    strcpy(p, "hello");
-    free(p);
-    printf("%s\n", p);
-}
-
-void delta() {
-    int *p = NULL;
+void beta() {
+    int *p = (int*)malloc(sizeof(int));
     *p = 10;
+    free(p);
+    printf("%d\n", *p);
 }
 
-void epsilon() {
-    int *p = (int*)malloc(100 * sizeof(int));
-    p[0] = 1;
-}
-
-void zeta() {
+void gamma() {
     int x = INT_MAX;
-    int y = x + 5;
+    int y = x + 100;
     printf("%d\n", y);
+}
+
+void delta(char *input) {
+    printf(input);
 }
 
 int main(int argc, char *argv[]) {
     if (argc > 1) {
         alpha(argv[1]);
-        beta(argv[1]);
+        delta(argv[1]);
     }
 
+    beta();
     gamma();
-    delta();
-    epsilon();
-    zeta();
 
     return 0;
 }
